e088870cb421ad44d62a91e3c004cc3fb4f4c4bf
Alex1304 <mirandaa1304@gmail.com>
2020-12-04 15:59:55 +0100

    Created website, changed groupId

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Botrino
 
 A simple yet powerful framework to develop, configure and run Discord bots based on Discord4J.
+
+**This project is still a work-in-progress, the library is not yet published to Maven Central. This README as well as
+the documentation website will **
---
+++ website/docusaurus.config.js
@@ -0,0 +1,105 @@
+module.exports = {
+  title: 'Botrino',
+  tagline: 'A simple yet powerful framework to develop, configure and run Discord bots based on Discord4J.',
+  url: 'https://botrino.alex1304.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'Alex1304', // Usually your GitHub org/user name.
+  projectName: 'botrino', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'Botrino',
+      logo: {
+        alt: 'Botrino Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        },
+        //{to: 'blog', label: 'Blog', position: 'left'},
+        {
+          href: 'https://github.com/Alex1304/botrino',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Style Guide',
+              to: 'docs/',
+            },
+            {
+              label: 'Second Doc',
+              to: 'docs/doc2/',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            /*{
+                label: 'Blog',
+                to: 'blog',
+            },*/
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/Alex1304/botrino/edit/main/website/',
+        },
+        /*blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },*/
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
